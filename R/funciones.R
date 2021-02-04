cbc1783c40ec5a7532f9dbf08974a12643330185
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2021-02-04 17:37:18 -0500

    update

--- R/funciones.R
+++ R/funciones.R
@@ -1,100 +1,100 @@
-identificar_cala <- function(data = data, directory = getwd(), thres = 0.51, neurons = 4, loops = 10){
-
-  require(nnet)
-
-  data$date <- as.POSIXct(strptime(as.character(data$Date), format = "%Y-%m-%d %H:%M:%S"))
-  data$date_GMT <- as.POSIXct(data$Date,tz='GMT')
-  data$Cod_Barco <- as.factor(data$Cod_Barco)
-  #data$Clase_Emision <- as.factor(data$Clase_Emision)
-  #data$Zona <- as.factor(data$Zona)
-  data$Puerto_0_Mar_1 <- as.factor(data$id)
-  #data$Cala <- as.factor(data$Cala)
-  #data$Primera_Cala <- as.factor(data$Primera_Cala)
-  #data$Cod_Viaje_VMS <- as.factor(data$Cod_Viaje_VMS)
-  #data$Cod_Viaje_Cruz <- as.factor(data$Cod_Viaje_Cruz)
-  #data$Flota <- as.factor(data$Flota)
-  #data$Pesca_Viaje <- as.factor(data$Pesca_Viaje)
-
-  fechas <- unclass(as.POSIXlt(data$date))
-  data$hora <- fechas$hour + fechas$min/60 + fechas$sec/3600
-
-  data$hora_transf <- cos(data$hora*pi/12)
-  data$cambio_rumbo_transf <- cos(data$Cambio_Rumbo_Calc*pi/180)
-  data$Cod_Viaje_VMS <- paste0(data$Cod_Barco, "-",data$trip)
-  data$Dif_Tiempo <- data$Time
-  data$Puerto_0_Mar_1 <- data$id
-  cod_viajes <- unique(data$Cod_Viaje_VMS)
-
-  data$Change_Speed_1 <- rep(NA,dim(data)[1])
-  data$Change_Speed_2 <- rep(NA,dim(data)[1])
-  data$Acel_1 <- rep(NA,dim(data)[1])
-  data$Acel_2 <- rep(NA,dim(data)[1])
-  data$Cambio_Rumbo_Tiempo <- rep(NA,dim(data)[1])
-
-  for (i in seq_along(cod_viajes)){
-    lignes <- which(data$Cod_Viaje_VMS == cod_viajes[i])
-    data$Change_Speed_1[lignes] <- c(NA,diff(data$Vel_Cal[lignes]))
-    data$Change_Speed_2[lignes] <- c(diff(data$Vel_Cal[lignes]),NA)
-    Dif_T_3 <- c(NA,data$Dif_Tiempo[lignes]) + c(data$Dif_Tiempo[lignes],NA)
-    data$Acel_1[lignes] <- data$Change_Speed_1[lignes]/Dif_T_3[1:(length(Dif_T_3)-1)]
-    data$Acel_2[lignes] <-  c(data$Acel_1[lignes[-1]],NA) #c(data$Acel.1[lignes[2:length(lignes)]],NA)
-    data$Cambio_Rumbo_Tiempo[lignes] <- data$cambio_rumbo_transf[lignes]/Dif_T_3[1:(length(Dif_T_3)-1)]
-  }
-
-  #data[which(is.na(data$Acel_2)), "Cod_Viaje_VMS"]
-  #data[data$Cod_Viaje_VMS %in%  "CO-12974-PM-8",]
-
-  variables <- c("Dist_Emisiones","Vel_Cal","Change_Speed_1","Change_Speed_2","Acel_1","Acel_2","hora_transf",
-                 "cambio_rumbo_transf","Cambio_Rumbo_Tiempo")
-
-  ind_change_speed_1 <- which(is.na(data[,variables[3]]))
-  ind_change_speed_2 <- which(is.na(data[,variables[4]]))
-
-  data$calas <- 0
-  data0 <- data
-  # data0$Cala <- 0
-  data <- data[-c(ind_change_speed_1,ind_change_speed_2),]
-
-  data2 <- data[,variables]
-
-  names(data2) <- c("Dist_Emisiones","Vel.Cal","Change_Speed_1","Change_Speed_2","Acel.1","Acel.2","hora.transf",
-                    "cambio_rumbo_transf","Cambio.Rumbo.Tiempo")
-
-  variables    <- c("Dist_Emisiones","Vel.Cal","Change_Speed_1","Change_Speed_2","Acel.1","Acel.2","hora.transf",
-                    "cambio_rumbo_transf","Cambio.Rumbo.Tiempo")
-
-  data_scaled <- scale(data2[,variables],center=TRUE,scale=TRUE)
-  if(is.null(data$match)) {data$match <- NA}
-
-  covariables <- c("Cod_Barco","Lon","Lat","Puerto_0_Mar_1","Dist_Emisiones","Dif_Tiempo",#"Name_Vessel",
-                   "calas","trip",
-                   "date", "dist_costa","match")
-
-  ##
-  data_scaled <- cbind(data[,covariables],data_scaled)
-
-
-  predichas <- matrix(NA,nrow=dim(data_scaled)[1],ncol=loops)
-  for (ii in 1:loops){
-
-    namefile = system.file("result_nnet", paste0("ann_year_loop_",ii,"_neurons_4.RData"),package = "tasaR")
-    #namefile = paste0(directory,"/result_nnet/ann_year_loop_",ii,"_neurons_",neurons)
-    #load(file = paste0(namefile, ".RData"))
-    load(namefile)
-    predicted <- predict(best.net,data_scaled)
-    predichas[,ii] <- predicted[,2]
-
-  }
-  sets <- as.numeric(apply(predichas,1,mean) > thres)
-
-  data0$calas[-c(ind_change_speed_1,ind_change_speed_2)] <- sets
-
-  if(!is.na(data$match[1])){
-    data01 <- posiciones_cala(data0)
-  }
-
-  return(data0)
-}
+# identificar_cala <- function(data = data, directory = getwd(), thres = 0.51, neurons = 4, loops = 10){
+#
+#   require(nnet)
+#
+#   data$date <- as.POSIXct(strptime(as.character(data$Date), format = "%Y-%m-%d %H:%M:%S"))
+#   data$date_GMT <- as.POSIXct(data$Date,tz='GMT')
+#   data$Cod_Barco <- as.factor(data$Cod_Barco)
+#   #data$Clase_Emision <- as.factor(data$Clase_Emision)
+#   #data$Zona <- as.factor(data$Zona)
+#   data$Puerto_0_Mar_1 <- as.factor(data$id)
+#   #data$Cala <- as.factor(data$Cala)
+#   #data$Primera_Cala <- as.factor(data$Primera_Cala)
+#   #data$Cod_Viaje_VMS <- as.factor(data$Cod_Viaje_VMS)
+#   #data$Cod_Viaje_Cruz <- as.factor(data$Cod_Viaje_Cruz)
+#   #data$Flota <- as.factor(data$Flota)
+#   #data$Pesca_Viaje <- as.factor(data$Pesca_Viaje)
+#
+#   fechas <- unclass(as.POSIXlt(data$date))
+#   data$hora <- fechas$hour + fechas$min/60 + fechas$sec/3600
+#
+#   data$hora_transf <- cos(data$hora*pi/12)
+#   data$cambio_rumbo_transf <- cos(data$Cambio_Rumbo_Calc*pi/180)
+#   data$Cod_Viaje_VMS <- paste0(data$Cod_Barco, "-",data$trip)
+#   data$Dif_Tiempo <- data$Time
+#   data$Puerto_0_Mar_1 <- data$id
+#   cod_viajes <- unique(data$Cod_Viaje_VMS)
+#
+#   data$Change_Speed_1 <- rep(NA,dim(data)[1])
+#   data$Change_Speed_2 <- rep(NA,dim(data)[1])
+#   data$Acel_1 <- rep(NA,dim(data)[1])
+#   data$Acel_2 <- rep(NA,dim(data)[1])
+#   data$Cambio_Rumbo_Tiempo <- rep(NA,dim(data)[1])
+#
+#   for (i in seq_along(cod_viajes)){
+#     lignes <- which(data$Cod_Viaje_VMS == cod_viajes[i])
+#     data$Change_Speed_1[lignes] <- c(NA,diff(data$Vel_Cal[lignes]))
+#     data$Change_Speed_2[lignes] <- c(diff(data$Vel_Cal[lignes]),NA)
+#     Dif_T_3 <- c(NA,data$Dif_Tiempo[lignes]) + c(data$Dif_Tiempo[lignes],NA)
+#     data$Acel_1[lignes] <- data$Change_Speed_1[lignes]/Dif_T_3[1:(length(Dif_T_3)-1)]
+#     data$Acel_2[lignes] <-  c(data$Acel_1[lignes[-1]],NA) #c(data$Acel.1[lignes[2:length(lignes)]],NA)
+#     data$Cambio_Rumbo_Tiempo[lignes] <- data$cambio_rumbo_transf[lignes]/Dif_T_3[1:(length(Dif_T_3)-1)]
+#   }
+#
+#   #data[which(is.na(data$Acel_2)), "Cod_Viaje_VMS"]
+#   #data[data$Cod_Viaje_VMS %in%  "CO-12974-PM-8",]
+#
+#   variables <- c("Dist_Emisiones","Vel_Cal","Change_Speed_1","Change_Speed_2","Acel_1","Acel_2","hora_transf",
+#                  "cambio_rumbo_transf","Cambio_Rumbo_Tiempo")
+#
+#   ind_change_speed_1 <- which(is.na(data[,variables[3]]))
+#   ind_change_speed_2 <- which(is.na(data[,variables[4]]))
+#
+#   data$calas <- 0
+#   data0 <- data
+#   # data0$Cala <- 0
+#   data <- data[-c(ind_change_speed_1,ind_change_speed_2),]
+#
+#   data2 <- data[,variables]
+#
+#   names(data2) <- c("Dist_Emisiones","Vel.Cal","Change_Speed_1","Change_Speed_2","Acel.1","Acel.2","hora.transf",
+#                     "cambio_rumbo_transf","Cambio.Rumbo.Tiempo")
+#
+#   variables    <- c("Dist_Emisiones","Vel.Cal","Change_Speed_1","Change_Speed_2","Acel.1","Acel.2","hora.transf",
+#                     "cambio_rumbo_transf","Cambio.Rumbo.Tiempo")
+#
+#   data_scaled <- scale(data2[,variables],center=TRUE,scale=TRUE)
+#   if(is.null(data$match)) {data$match <- NA}
+#
+#   covariables <- c("Cod_Barco","Lon","Lat","Puerto_0_Mar_1","Dist_Emisiones","Dif_Tiempo",#"Name_Vessel",
+#                    "calas","trip",
+#                    "date", "dist_costa","match")
+#
+#   ##
+#   data_scaled <- cbind(data[,covariables],data_scaled)
+#
+#
+#   predichas <- matrix(NA,nrow=dim(data_scaled)[1],ncol=loops)
+#   for (ii in 1:loops){
+#
+#     namefile = system.file("result_nnet", paste0("ann_year_loop_",ii,"_neurons_4.RData"),package = "tasaR")
+#     #namefile = paste0(directory,"/result_nnet/ann_year_loop_",ii,"_neurons_",neurons)
+#     #load(file = paste0(namefile, ".RData"))
+#     load(namefile)
+#     predicted <- predict(best.net,data_scaled)
+#     predichas[,ii] <- predicted[,2]
+#
+#   }
+#   sets <- as.numeric(apply(predichas,1,mean) > thres)
+#
+#   data0$calas[-c(ind_change_speed_1,ind_change_speed_2)] <- sets
+#
+#   if(!is.na(data$match[1])){
+#     data01 <- posiciones_cala(data0)
+#   }
+#
+#   return(data0)
+# }
 
 
 seleccionar_vms <- function(data, ...){
@@ -121,7 +121,7 @@ posiciones_cala <- function(data){
   data$Lat_calas <- NA
   data$calas[is.na(data$calas)] <- 0 # observar NAs
 
-  id_calas <- lapply(split(data, data$trip, drop = TRUE), function(x){
+  id_calas <- lapply(split(data, data$Cod_Viaje_VMS, drop = TRUE), function(x){
 
     if(sum(x$calas) > 0){
       ncalas   <-  1:(length(which(diff(which(x$calas == 1)) > 1)) + 1)
