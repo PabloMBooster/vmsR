52ee44748d60bb6f81f5b2022de42045ffee9e45
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2021-02-12 17:47:21 -0500

    update

--- R/projet.R
+++ R/projet.R
@@ -1,6 +1,6 @@
 projet <- function(data, proj4 = NULL, area = NULL){
   require(sp)
-
+  options(warn = - 1)
   xy <- data[c("Lon","Lat")]
   data[c("LONGITUDE_M","LATITUDE_M")] <- coordinates(spTransform(SpatialPointsDataFrame(xy, data,
                                                                                         proj4string = CRS(proj4),
--- R/unprojet.R
+++ R/unprojet.R
@@ -1,7 +1,6 @@
 unprojet <- function(data, proj4 = NULL, area = NULL){
-
   require(sp)
-
+  options(warn = - 1)
   xy <- data[c("lon2","lat2")]
   data[c("LONGITUDE","LATITUDE")] <- coordinates(spTransform(SpatialPointsDataFrame(xy,data,
                                                                                     proj4string = CRS(area)),
